5485375dc440a461e64ad86995596ae787d851b0
Alec Magnet <amagnet@gmail.com>
2021-08-18 19:58:16 -0400

    completed deliverables

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,3 +1,13 @@
 class Magazine < ActiveRecord::Base
-  
+	has_many :subscriptions
+	has_many :readers, through: :subscriptions  
+
+	def email_list
+		emails_arr = self.readers.map {|r| r.email}
+		emails_arr.join(";")
+	end
+
+	def self.most_popular
+		all.sort {|a, b| a.subscriptions.count <=> b.subscriptions.count}.last
+	end
 end
--- app/models/reader.rb
+++ app/models/reader.rb
@@ -1,3 +1,18 @@
 class Reader < ActiveRecord::Base
-  
+	has_many :subscriptions
+	has_many :magazines, through: :subscriptions
+
+	def subscribe(magazine, price)
+		Subscription.create(magazine_id: magazine.id, reader_id: self.id, price: price)
+	end
+
+	def total_subscription_price
+		price_arr = self.subscriptions.map {|s| s.price }
+		price_arr.sum
+	end
+
+	def cancel_subscription(magazine)
+		to_cancel = self.subscriptions.find_by magazine_id: magazine.id
+		Subscription.destroy(to_cancel.id)
+	end
 end
